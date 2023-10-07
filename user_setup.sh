757bede913ba26e68fa0a56d7ab0c8c35ca57f03
Fahd-DevOps <fahdkhaled777@gmail.com>
2023-10-07 23:34:40 +0300

    add userfile

---
+++ user_setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Create new user
+sudo useradd -m -s /bin/bash fahd
+
+# Set password for new user
+echo 'fahd:123@fk' | sudo chpasswd
+
+# Add new user to sudoers
+echo 'fahd ALL=(ALL) NOPASSWD:ALL' | sudo tee -a /etc/sudoers
+
+# Update SSH configuration to use new user
+sudo sed -i 's/^.*[Dd]efaultUser.*$/DefaultUser fahd/' /etc/ssh/sshd_config
+sudo systemctl restart sshd
+
+# Disable vagrant user
+# sudo usermod --lock vagrant
