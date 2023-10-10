76ff4298f7a26ffc0ee66afb5038a416e08b3729
Fahd-DevOps <fahdkhaled777@gmail.com>
2023-10-10 16:01:33 +0300

    edit user

--- user_setup.sh
+++ user_setup.sh
@@ -10,8 +10,8 @@ echo 'fahd:123@fk' | sudo chpasswd
 echo 'fahd ALL=(ALL) NOPASSWD:ALL' | sudo tee -a /etc/sudoers
 
 # Update SSH configuration to use new user
-sudo sed -i 's/^.*[Dd]efaultUser.*$/DefaultUser fahd/' /etc/ssh/sshd_config
-sudo systemctl restart sshd
+# sudo sed -i 's/^.*[Dd]efaultUser.*$/DefaultUser fahd/' /etc/ssh/sshd_config
+# sudo systemctl restart sshd
 
 # Disable vagrant user
 # sudo usermod --lock vagrant
